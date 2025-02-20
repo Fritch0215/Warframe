b2254dcf5622a7145e313f267996ca64748e3ecf
Group2-System <erreldungca069@gmail.com>
2025-02-20 11:14:52 +0800

    sadasdasd

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author CL3-PC09
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
