bd63ba517320d66cde22d4d3306cd121adaa26fc
Group2-System <erreldungca069@gmail.com>
2025-03-03 11:09:49 +0800

    try

---
+++ wrex/src/main/java/com/mycompany/wrex/Wrex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wrex;
+
+/**
+ *
+ * @author CL3-PC09
+ */
+public class Wrex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
